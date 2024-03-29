2ae0bcc249168124afcd33a4545431c7ad94ede8
Lyra Ganem <lyraganemt@student.douglascollege.ca>
2024-03-28 17:01:45 -0700

    adding initial files

---
+++ index.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const express = require('express')
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+// Create a Schema object
+
+// Create a Model object
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/form.html")
+});
+
+app.post('/', async (req, res) => {
+  // get the data from the form
+
+  // connect to the database and log the connection
+
+  // add the data to the database
+
+  // send a response to the user
+  res.send(`<h1>Document  Added</h1>`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
